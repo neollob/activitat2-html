2593f92d1ec166c7ec6fbcff063063db338a5d25
Dani <neollob@yahoo.es>
2019-05-01 23:22:09 +0200

    3rd commit - 1a Revisio

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,23 @@
 (function () {
     generateProverb();
-    var today = new Date()
-    var curHr = today.getHours()
-    try {
-        if (curHr < 12) {
-            document.getElementById('greeting').innerHTML = 'Bon dia'
-        } else if (curHr < 18) {
-            document.getElementById('greeting').innerHTML = 'Bona tarda'
-        } else {
-            document.getElementById('greeting').innerHTML = 'Bona nit'
-        }
-    } catch{ }
+    greetings();
 })();
+
+function greetings(){
+    var curHr = new Date().getHours()
+    var morning = ('Bon dia');
+    var afternoon = ('Bona tarda');
+    var evening = ('Bona nit');
+    var greeting;
+
+    try {
+        if (curHr < 14) {
+            greeting = morning
+        } else if (curHr < 21) {
+            greeting = afternoon
+        } else {
+            greeting = evening
+        }
+        document.getElementById('greeting').innerHTML = greeting
+    } catch{ }
+}
