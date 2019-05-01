bd0da464e0ddacf72ffb947f1cbd1a0932f2e82b
Dani <neollob@yahoo.es>
2019-05-01 21:51:26 +0200

    2nd commit - Greeting added

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,14 @@
 (function () {
     generateProverb();
+    var today = new Date()
+    var curHr = today.getHours()
+    try {
+        if (curHr < 12) {
+            document.getElementById('greeting').innerHTML = 'Bon dia'
+        } else if (curHr < 18) {
+            document.getElementById('greeting').innerHTML = 'Bona tarda'
+        } else {
+            document.getElementById('greeting').innerHTML = 'Bona nit'
+        }
+    } catch{ }
 })();
